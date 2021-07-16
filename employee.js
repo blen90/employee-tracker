fb790a43bc1608bf34942fa72abbc297d623b7bd
Blenda Orellana <blen.or90@gmail.com>
2021-07-16 17:53:02 -0500

    added function add employee, changed seed file, added view all employees function

--- employee.js
+++ employee.js
@@ -14,10 +14,6 @@ const connection = mysql.createConnection( {
     }
 );
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connect as id ${connection.threadId}`)
-});
 
 
 const start = () => {
@@ -71,19 +67,39 @@ const start = () => {
 
 //Functions depending on the option the user chose
 
+
 function viewAllEmployees() {
-     connection.query('SELECT * FROM employee', (err, res) => {
-         if (err){
-             throw err;
-         }
+    const query = 
+    "SELECT * FROM employee";
+    // "LEFT JOIN role ON role.id" +
+    // "LEFT JOIN department ON department.id = role.department_id";
+
+    connection.query(query, (err, res) => {
+        if(err)
+        throw err;
+    {
         console.table(res);
-            
-     })
-     start();
-        }
+    
+    }
+    start();
+    })
+}
+// function viewAllEmployees() {
+//     const query = connection.query(
+//         'SELECT * FROM employee', 
+//         (err, res) => {
+//             if (err) 
+//             throw err;
+
+//          res.forEach(employee => {
+//             console.table(`ID: ${employee.id} Name:  ${employee.first_name} Last Name:${employee.last_name} Role Id:  ${employee.role_id} Manager Id: ${employee.manager_id}`);
+//          })     
+
+//         start();
+//      })
+//         }
         
      
-
 
 
 
@@ -93,17 +109,97 @@ function viewByDepartment() {
             throw err;
         }
        console.table(res);
-           
+
+       start();
     })
-    start();
+   
        }
 
+    //    let getManagers = () => {
+    //     let query = 'SELECT id,first_name, last_name, manager_id,role_id FROM employee';
+    //     query += ' WHERE manager_id IS NULL;';
+    //     return new Promise((resolve, reject) => {
+    //         connection.query(query, (err, res) => {
+    //             if (err) {
+    //                 reject(new Error(err.message));
+    //             }
+    //             let employees = [];
+    //             res.forEach(({ id, first_name, last_name, manager_id, roleId }) => {
+    //                 let emp = new Employee(id, first_name, last_name, manager_id, roleId);
+    //                 employees.push(emp);
+    //             });
+    //             resolve(employees);
+    //         })
+    //     });
+    // };
+    
+    
+    
+    // case 'View All Managers':
+    //                     getManagers()
+    //                         .then((response) => {
+    //                             displayResults(response);
+    //                         })
+    //                         .then(() => runSearch())
+    //                         .catch((err) => console.error('Promise rejected:', err));
+    //                     break;
 
 
-// viewbyManager();
+// function viewbyManager(managerId) {
+//     connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.id FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;', managerId, (err, res) => {
+//         if (err){
+//             throw err;
+//         }
+//        console.table(res);
+       
+//        start();
+//     })
+   
+//        }
 
 
-// addEmployee();
+
+
+function addEmployee() {
+    inquirer.prompt ([
+        {
+            type:"input",
+            message:"What is the employee's first name?",
+            name:"name"
+        },
+        {
+            type:"input",
+            message:"What is the employee's last name?",
+            name:"lastName"
+        },
+        {
+            type:"input",
+            message:"What is the employee's role id?",
+            name:"roleId"
+        },
+        {
+            type:"input",
+            message:"What is is the employee's manager id?",
+            name:"managerId"
+        }
+
+
+    ]).then(answers => {
+        connection.query("INSERT INTO employee SET ?", 
+        {
+            first_name: answers.name,
+            last_name: answers.lastName,
+            role_id: answers.roleId,
+            manager_id: answers.managerId
+        }, (err, res) => {
+            if (err)
+            throw err;
+        
+        })
+
+        start();
+    });
+}
 
 
 // removeEmployee();
@@ -117,6 +213,11 @@ function viewByDepartment() {
 //Connection and start of app
 
 
-
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connect as id ${connection.threadId}`)
+});
 
 start();
+
+
--- employeeSeed.sql
+++ employeeSeed.sql
@@ -23,41 +23,41 @@ CREATE TABLE role (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL NOT NULL,
-    department_id INTEGER
+    department_id INTEGER,
+
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 INSERT INTO role(title, salary, department_id)
     VALUES 
     ("Sales Manager", 175000, 1),
-    ("Salesperson", 125000, 1),
     ("Engineer Manager", 175000, 2),
-    ("Engineer", 115000, 2),
-    ("IT Team Lead", 95000, 3),
-    ("IT Support", 80000, 3),
+    ("IT Manager", 175000, 3),
     ("Legal Manager", 175000, 4),
-    ("Lawyer", 75000, 4),
-    ("Accounting Manager", 170000, 5),
-    ("Accountant", 110000, 5),
-    ("HR Generalist", 150000,6),
-    ("Recruiter", 100000, 6);
+    ("Accounting Manager", 175000, 5),
+    ("HR Manager", 175000,6);
+
 
 CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
     role_id INTEGER(10) NOT NULL,
-    manager_id INTEGER(10) NOT NULL
+    manager_id INTEGER(10) NOT NULL,
 
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
     VALUES 
-    ("Blenda", "Orellana", 1, 1),
+    ("Blenda", "Orellana", 1, 1), 
     ("Oso", "Orellana", 2, 2),
     ("Wolfgang", "Orellana", 3, 3),
     ("Balam", "Orellana", 4, 4),
     ("Nena", "Orellana", 5, 5),
     ("Maya", "Orellana", 6, 6);
+ 
 
 
 
